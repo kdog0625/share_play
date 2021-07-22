1b35e24b6dd4878977898021d31c4f5f1ce18cf4
犬飼宏輔 <kdoooog34@gmail.com>
2021-07-23 00:10:34 +0900

    投稿に対してバリデーションを設定

--- laravel/app/Http/Controllers/SharesController.php
+++ laravel/app/Http/Controllers/SharesController.php
@@ -45,22 +45,7 @@ class SharesController extends Controller
 
     //シェアハウスを登録するデータのまとめ
     public function shares_create_colum($share, $request) {
+        $share->fill($request->all());
         $share->admin_users_id = $request->user()->id;
-        $share->shara_name = $request->shara_name;
-        $share->prefecture_name = $request->prefecture_name;
-        $share->municipality_name = $request->municipality_name;
-        $share->supplementary_address = $request->supplementary_address;
-        $share->share_picture = $request->share_picture;
-        $share->share_text = $request->share_text;
-        $share->exchange_about = $request->exchange_about;
-        $share->build_age = $request->build_age;
-        $share->private_room = $request->private_room;
-        $share->dormitory_room = $request->dormitory_room;
-        $share->access_about = $request->access_about;
-        $share->friends_exchange = $request->friends_exchange;
-        $share->friend_desc = $request->friend_desc;
-        $share->gender_ratio = $request->gender_ratio;
-        $share->jp_ov_ratio = $request->jp_ov_ratio;
-        $share->age_ratio = $request->age_ratio;
     }
 }
--- laravel/app/Models/Share.php
+++ laravel/app/Models/Share.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Share extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'shara_name',
+        'prefecture_name',
+        'municipality_name',
+        'supplementary_address',
+        'share_picture',
+        'exchange_about',
+        'build_age',
+        'private_room',
+        'dormitory_room',
+        'access_about',
+        'friends_exchange',
+        'friend_desc',
+        'gender_ratio',
+        'jp_ov_ratio',
+        'age_ratio',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Model\Admin_User');
+    }
 }
