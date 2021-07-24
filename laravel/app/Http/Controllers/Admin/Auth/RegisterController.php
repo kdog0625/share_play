38e299dfaf6909216e081bc93c304043d4cc8b6a
犬飼宏輔 <kdoooog34@gmail.com>
2021-07-24 23:09:51 +0900

    追記でAdminを大文字に変更

--- laravel/app/Http/Controllers/Admin/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -31,7 +31,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::Admin;
+    protected $redirectTo = RouteServiceProvider::ADMIN;
 
     /**
      * Create a new controller instance.
